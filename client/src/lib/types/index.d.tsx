3ec1da7c9980d7238d70a43603addf4fc76e23ba
sudarshaMug <sudarshamugiti@gmail.com>
2025-04-01 16:31:10 +0530

    end of client code

---
+++ client/src/lib/types/index.d.tsx
@@ -0,0 +1,12 @@
+export type Activity = {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  isCancelled: boolean;
+  category: string;
+  city: string;
+  venue: string;
+  latitude: number;
+  longitude: number;
+}
