49af945816348e2ce0361ced433b56ee4742b9e7
sudarshaMug <sudarshamugiti@gmail.com>
2025-04-03 17:09:08 +0530

    End of CRUD in .NET

---
+++ Application/Activities/Commands/DeleteActivity.cs
@@ -0,0 +1,26 @@
+using System;
+using Domain;
+using MediatR;
+using Persistance;
+
+namespace Application.Activities.Commands;
+
+public class DeleteActivity
+{
+    public class Command : IRequest
+    {
+        public required string Id{get; set;}
+    }
+    public class Handler(AppDbContext context) : IRequestHandler<Command>
+    {
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            var activity = await context.Activities.FindAsync([request.Id], cancellationToken);
+            if (activity == null) throw new Exception("Activity cannot be found");
+
+            context.Remove(activity);
+
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
